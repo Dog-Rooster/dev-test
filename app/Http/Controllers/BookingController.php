b8a5a353cc8e7471c3e3225a7d6a45c08a2fb4aa
patrivkpan-jpg <patrivkpan98@gmail.com>
2024-09-05 17:47:43 +0800

    Prevent double booking and allows users to set timezone

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -6,6 +6,7 @@ use App\Models\Booking;
 use App\Models\Event;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use DateTimeZone;
 
 class BookingController extends Controller
 {
@@ -18,47 +19,65 @@ class BookingController extends Controller
 
     public function store(Request $request, $eventId)
     {
-        $event = Event::findOrFail($eventId);
+        // TODO: Add timezone to booking table; PRIO: 1
+        // TODO: Add backend validation for double booking; PRIO: 2
+        $bookingTimeslot = Carbon::createFromFormat('Y-m-d H:i', 
+            $request->input('booking_date') . ' ' . $request->input('booking_time'), 
+            $request->input('booking_timezone'))->setTimezone('UTC');
 
         $booking = new Booking();
         $booking->attendee_name = $request->input('attendee_name');
         $booking->attendee_email = $request->input('attendee_email');
         $booking->event_id = $eventId;
-        $booking->booking_date = $request->input('booking_date');
-        $booking->booking_time = $request->input('booking_time');
+        $booking->booking_date = $bookingTimeslot->toDateString();
+        $booking->booking_time = $bookingTimeslot->toTimeString();
         $booking->save();
 
+        $booking->booking_date =  $request->input('booking_date');
+        $booking->booking_time = $request->input('booking_time');
         return view('bookings.thank-you', ['booking' => $booking]);
     }
 
     public function create(Request $request, $eventId)
     {
+        // TODO: Move to resource; PRIO: 2
         $event = Event::findOrFail($eventId);
         $selectedDate = $request->input('booking_date', now()->toDateString());
+        $selectedTimezone = $request->input('booking_timezone', env('APP_TIMEZONE', 'UTC'));
 
-        $timeSlots = $this->generateTimeSlots($selectedDate);
+        $timeSlots = $this->generateTimeSlots($selectedDate, $selectedTimezone, $event);
+        $timezones = DateTimeZone::listIdentifiers(DateTimeZone::ALL);
 
-        return view('bookings.calendar', compact('event', 'timeSlots', 'selectedDate'));
+        return view('bookings.calendar', compact('event', 'timeSlots', 'timezones', 'selectedDate', 'selectedTimezone'));
     }
 
-    private function generateTimeSlots($date)
+    private function generateTimeSlots($date, $timezone, $event)
     {
         $startOfDay = Carbon::parse($date)->startOfDay();
         $endOfDay = Carbon::parse($date)->startOfDay()->addHours(24);
-        $interval = 30; // 30 minutes per time block
+        $interval = $event->duration;
+        $bookings = Booking::select('booking_date', 'booking_time')
+            ->whereBetween('booking_date', [Carbon::parse($date)->subHours(24), Carbon::parse($date)->addHours(24)])
+            ->get()
+            ->all();
 
+        $bookedTimeSlots = [];
+        foreach ($bookings as $booking) {
+            $bookedTimeSlots[] = Carbon::parse($booking->booking_date . ' ' . $booking->booking_time)->setTimezone($timezone)->toDateTimeString();
+        }
+        
         $timeSlots = [];
-
         while ($startOfDay < $endOfDay) {
             $end = $startOfDay->copy()->addMinutes($interval);
 
             $timeSlots[] = [
                 'time' => $startOfDay->format('H:i'),
+                'booked' => in_array($startOfDay, $bookedTimeSlots)
             ];
 
             $startOfDay = $end;
         }
-
+        
         return $timeSlots;
     }
 }
--- resources/views/bookings/calendar.blade.php
+++ resources/views/bookings/calendar.blade.php
@@ -3,14 +3,26 @@
         @if (!request('booking_time'))
             <h1 class="text-2xl font-bold mb-6">Select a Time Slot for {{ $event->name }}</h1>
 
-            <div class="mb-4">
-                <form action="{{ route('bookings.create', $event->id) }}" method="GET">
+            <form id="booking_details_form" action="{{ route('bookings.create', $event->id) }}" method="GET">
+                <div class="mb-4">
+                    <label for="booking_timezone" class="block font-medium text-gray-700">Select Timezone:</label>
+                    <select name="booking_timezone" id="booking_timezone" class="border rounded p-2" 
+                        onchange="document.getElementById('booking_details_form').submit()">
+                        @foreach ($timezones as $timezone)
+                            <option value="{{ $timezone }}" {{ ($timezone === request('booking_timezone', 'UTC')) ? 'selected' : '' }}>
+                                {{ $timezone }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="mb-4">
                     <label for="booking_date" class="block font-medium text-gray-700">Select Date:</label>
                     <input type="date" name="booking_date" id="booking_date" class="border rounded p-2"
-                        value="{{ $selectedDate }}" required>
-                    <button type="submit" class="ml-4 px-4 py-2 bg-blue-600 text-white rounded">Change Date</button>
-                </form>
-            </div>
+                        value="{{ $selectedDate }}" required 
+                        onchange="document.getElementById('booking_details_form').submit()">
+                </div>
+            </form>
 
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 @foreach ($timeSlots as $time)
@@ -18,9 +30,11 @@
                         <span class="text-lg font-medium">{{ $time['time'] }}</span>
                         <form action="{{ route('bookings.create', $event->id) }}" method="GET" class="mt-2">
                             <input type="hidden" name="booking_date" value="{{ $selectedDate }}">
+                            <input type="hidden" name="booking_timezone" value="{{ $selectedTimezone }}">
                             <input type="hidden" name="booking_time" value="{{ $time['time'] }}">
                             <button type="submit"
-                                class="w-full px-4 py-2 bg-blue-600 text-white rounded">Select</button>
+                                class="w-full px-4 py-2 rounded bg-blue-600 text-white {{ ($time['booked']) ? 'opacity-50 cursor-not-allowed' : '' }}" 
+                                {{ ($time['booked']) ? 'disabled' : '' }}>Select</button>
                         </form>
                     </div>
                 @endforeach
@@ -35,6 +49,7 @@
                     <p><strong>Time:</strong> {{ request('booking_time') }}</p>
                     <input type="hidden" name="booking_date" value="{{ request('booking_date') }}">
                     <input type="hidden" name="booking_time" value="{{ request('booking_time') }}">
+                    <input type="hidden" name="booking_timezone" value="{{ request('booking_timezone') }}">
 
                     <label for="attendee_name">Name:</label>
                     <input type="text" name="attendee_name" id="attendee_name" required>
